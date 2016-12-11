ffa0ed239e355f5b257e8ca623f72c03bc15f4c0
remyCESI <remy.vancamp@viacesi.fr>
2016-12-11 16:42:53 +0100

    interactif mise a jour 11/12/16

---
+++ saverInteractif.c
@@ -0,0 +1,280 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const Width 24
+const Lenght 80
+
+main (int 1argc ,char  *argv []) {
+
+image chargeimage (,)
+
+ 
+
+
+
+
+}
+
+
+//declaration the different function 
+
+
+//charge path image and saveit in the structure image
+image chargeImage(*image , path image){
+
+typedef struct images images{
+
+	FILE* avion_H;
+	FILE* avion_B;
+	FILE* avion_D;
+	FILE* avion_G;
+
+};
+
+
+FILE* AfficheFichier = NULL;
+AfficheFichier= fopen(char*"",char  "r"); //use a variable for simplify
+
+	if(AfficheFichier != NULL){
+
+		do{
+	fgetc()// read the caracter
+				  }
+
+
+while (fgetc () != EOF); //read the caracter while there is caracter
+
+	fclose(Affichefichier); // close file
+
+	else {
+
+	printf("lecture du fichier impossible ");//send a error message
+	     }
+
+
+}
+
+//complete the plane coordinate strcuture with the bytes
+
+coordoAvion remplissageAvion (image , *coordoAvion){
+	
+	typedef struct coordoAvion coordoAvion {
+
+		int  a1 = 4;
+		int  o1 = 1;
+
+		int  a2 = 3;
+		int  o2 = 2;
+
+		int  a3 = 4;
+		int  o3 = 2;
+
+		int  a4 = 5;
+		int  o4 = 2;
+
+		int  a5 = 2;
+		int  o5 = 3;
+
+		int  a6 = 4;
+		int  o6 = 3;
+
+		int  a7 = 6;
+		int  o7 = 3;
+
+		int  a8 = 4;
+		int  o8 = 4;
+
+		int  a9 = 4;
+		int  o9 = 5;
+
+		int  a10 = 3;
+		int  o10 = 6;
+
+		int  a11 = 4;
+		int  o11 = 6;
+			             	           }
+
+
+
+
+						      }
+
+// complete the static tab with the coordinate plane structure
+void remplissageTAB (*coordoAvion , *tabConsole){
+
+
+
+
+						}
+
+//change the coordinates of the planes for move it 
+int deplacement(*avionLoc, *coordoAvion){
+typedef struct control{
+
+		int control_H = 1;
+		int control_B = 4;
+		int control_D = 3;
+		int control_G = 2;
+
+		      }
+
+int toucheCtrl=0;
+scanf("tapez (1) en haut , (2) a gauche, (3) a droite, (4) en bas %d",toucheCtrl);
+  
+
+	if (toucheCtrl== 1){
+
+	        int  a1 = 4;
+                int  o1 = 1;
+
+                int  a2 = 3;
+                int  o2 = 2;
+
+                int  a3 = 4;
+                int  o3 = 2;
+
+                int  a4 = 5;
+                int  o4 = 2;
+
+                int  a5 = 2;
+                int  o5 = 3;
+
+                int  a6 = 4;
+                int  o6 = 3;
+
+                int  a7 = 6;
+                int  o7 = 3;
+
+                int  a8 = 4;
+                int  o8 = 4;
+
+                int  a9 = 4;
+                int  o9 = 5;
+
+                int  a10 = 3;
+                int  o10 = 6;
+
+                int  a11 = 4;
+                int  o11 = 6;
+
+
+
+			   }
+       else if(toucheCtrl == 2){
+
+	        int  a1 = 3;
+                int  o1 = 1;
+
+                int  a2 = 2;
+                int  o2 = 2;
+
+                int  a3 = 6;
+                int  o3 = 2;
+
+                int  a4 = 1;
+                int  o4 = 3;
+
+                int  a5 = 2;
+                int  o5 = 3;
+
+                int  a6 = 3;
+                int  o6 = 3;
+
+                int  a7 = 4;
+                int  o7 = 3;
+
+                int  a8 = 5;
+                int  o8 = 3;
+
+                int  a9 = 6;
+                int  o9 = 3;
+
+                int  a10 = 2;
+                int  o10 = 4;
+
+                int  a11 = 3;
+                int  o11 = 5;
+
+			       }
+
+	if(toucheCtrl == 3) {
+
+	        int  a1 = 4;
+                int  o1 = 1;
+
+                int  a2 = 1;
+                int  o2 = 2;
+
+                int  a3 = 5;
+                int  o3 = 2;
+
+                int  a4 = 1;
+                int  o4 = 3;
+
+                int  a5 = 2;
+                int  o5 = 3;
+
+                int  a6 = 3;
+                int  o6 = 3;
+
+                int  a7 = 4;
+                int  o7 = 3;
+
+                int  a8 = 5;
+                int  o8 = 3;
+
+                int  a9 = 6;
+                int  o9 = 3;
+
+                int  a10 = 5;
+                int  o10 = 4;
+
+                int  a11 = 4;
+                int  o11 = 5;
+
+			      }
+
+	else if(toucheCtrl == 4){
+
+	        int  a1 = 3;
+                int  o1 = 1;
+
+                int  a2 = 4;
+                int  o2 = 1;
+
+                int  a3 = 3;
+                int  o3 = 2;
+
+                int  a4 = 3;
+                int  o4 = 3;
+
+                int  a5 = 2;
+                int  o5 = 4;
+
+                int  a6 = 3;
+                int  o6 = 4;
+
+                int  a7 = 5;
+                int  o7 = 4;
+
+                int  a8 = 2;
+                int  o8 = 5;
+
+                int  a9 = 3;
+                int  o9 = 5;
+
+                int  a10 = 4;
+                int  o10 = 5;
+
+                int  a11 = 3;
+                int  o11 = 6;
+
+				 }
+
+return 0;
+ 				}
+
+//print to the screen tabConsole contaning the plane 
+void affichage ( *tabconsole){
+
+			     }
