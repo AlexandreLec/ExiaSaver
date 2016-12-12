671964cda870001a0989f579bc5a540800f9d7eb
remyCESI <remy.vancamp@viacesi.fr>
2016-12-12 10:28:02 +0100

    launcher.c

---
+++ launcher.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+main(int argc, char *argv){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+saver historique {
+
+--> il faut une recuperation de:-l'heure 
+				-la date
+				-le type de saver
+
+	
+
+
+}
+
+//random fonction use for know, what is the saver use
+random{
+
+	typedef struct saver{
+	
+	saverInteractif = 1;
+	saverDynamique = 2;
+	saverstatique = 3;
+
+			   };
+
+
+	compteurInteractif= 0 ;
+	compteurDynamique= 0 ;
+	compteurStatique= 0;
+	
+	
+	RANDOM (1 A 3)
+	
+	if(random==1){
+	compteurInteractif=compteurInteractif + 1;	
+		     }
+
+
+	else if(random==2){
+	compteurDynamique = compteurDynamique + 1;
+			  }
+
+	else (random==3) {
+		compteurStatique=compteurStatique+1;
+
+			}
+		}
+
+
+
