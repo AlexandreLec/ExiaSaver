111f4230cc5673dd6bff065ecc6dd749402d6adc
AlexandreLec <alexandre.lecomte@viacesi.fr>
2016-12-13 15:09:27 +0100

    v1.1

--- projet/exiasaver.c
+++ projet/exiasaver.c
@@ -3,48 +3,128 @@
 #include <time.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 int alea(int a, int b);
-char* picImg();
+char* picImg(char* pathImg);
 void start(char* nameExec, char* param);
+void history(char* type, char* param);
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	int nbRand;
-	char *pathImg;
-
-	system("clear");
-	
-	nbRand = alea(1,3);
-
-	switch(nbRand)
+	if(argc == 2 && (strcmp(argv[1], "-stats") == 0))
 	{
-		case 1:
-		//statique
-		pathImg = picImg();
-		start("statique",pathImg);
-		break;
+		printf("Choisissez un mode d'affichage des statistiques des screensavers lances");
+		//scanf("");
 		
-		case 2:
-		//dynamique
-		start("dynamique", NULL);
-		break;
+	}
+	
+	else if(argc == 1)
+	{
+		int nbRand;
+		char *path;
+		char *name;
+
+		system("clear");
+	
+		nbRand = alea(1,3);
+
+		switch(nbRand)
+		{
+			case 1:
+			//statique
+			path = getenv("EXIASAVER1_PBM");
+			name = picImg(path);
+			
+			history(" 1 ", name);
+			
+			strcat(path, name);
+			start("statique",path);
+			break;
 		
-		case 3:
-		//interactif
-		start("interactif", NULL);
-		break;
+			case 2:
+			//dynamique
+			history(" 2 ", name);
+			start("dynamique", NULL);
+			break;
+		
+			case 3:
+			//interactif
+			history(" 3 ", name);
+			start("interactif", NULL);
+			break;
+		}
+	}
+	
+	else
+	{
+		printf("Too many arguments\n");
 	}
 	
 	return 1;
 }
 
-char* picImg()
+int openHistory()
 {
-	char *pathImg;
+	int idH;
+
+	idH = open("history.txt", O_CREAT, S_IRUSR);
+		
+	if(idH==-1)
+	{
+		printf("fileHistory can't load");
+		return -1;
+	}
+	else
+	{
+		return idH;
+	}
+}
+
+void history(char* type, char* param)
+{
+	printf("%s", param);
+	
+	struct tm timeStart;
+	time_t sec;
+	
+	char *lineHistory;
+	lineHistory = (char*)malloc(sizeof("DD/MM/YYYY HH:MM:SS typetypetype parametreparametre"));
+	
+	sec = time(NULL);
+	timeStart = *localtime(&sec);
+	
+	strftime(lineHistory, sizeof("DD/MM/YYYY HH:MM:SS"), "%d/%m/%Y %H:%M:%S", &timeStart);
+
+	strcat(lineHistory,type);
+
+	strcat(lineHistory,param);
+	
+	FILE* history = fopen("history.txt","a+");
+	
+	if(history != NULL)
+	{
+		fprintf(history,"%s\n", lineHistory);
+		
+		fclose(history), history = NULL;
+		
+	}
+	else
+	{
+		printf("ERREUR");
+	}
+	
+	
+	free(lineHistory);	
+	
+}
+
+char* picImg(char* pathImg)
+{
 	char *nameImg;
 	char **tabImg;
 	
@@ -52,8 +132,6 @@ char* picImg()
 	int nbAlea=0;
 	
 	tabImg = (char**)malloc(10*(sizeof(char*)));
-	
-	pathImg = getenv("EXIASAVER1_PBM");
 	
 	struct dirent *contenuRep;
 	DIR *rep;
@@ -85,9 +163,7 @@ char* picImg()
 	
 	free(tabImg);
 	
-	strcat(pathImg, nameImg);
-	
-	return pathImg;
+	return nameImg;
 }
 
 int alea(int a, int b)
