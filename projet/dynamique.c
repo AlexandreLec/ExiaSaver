98ee7b7a5246317d7b6afcb519b7be5eefe0cc60
tapafe <arnaud.rigaut@viacesi.fr>
2016-12-14 14:56:59 +0100

    Update dynamique.c

--- projet/dynamique.c
+++ projet/dynamique.c
@@ -8,7 +8,6 @@
 #include <time.h>
 
 //Size of the console 24 lines 80 columns
-#define W_CONSOLE 80
 #define H_CONSOLE 24
 
 //Functions prototypes
@@ -23,11 +22,14 @@ int main (int argc, char *argv[])
 while (1 == 1)
 {
 
+        system("clear"); //clean
+
+
 	//Descriptor file
 	FILE* dessin = NULL;
-
+	int w_console = 10;
 	//Tab for bytes's line
-	char line[W_CONSOLE];
+	char line[w_console];
 
 	//Number of lines in the file
 	//int lineNb = 1;
@@ -39,12 +41,8 @@ while (1 == 1)
 	int *phei = &hei;
 	int *pwei = &wei;
 
-	system("clear"); //clean
-
 	int i, j;
 	int chrono;
-	int sertarien = 0;
-
 
 	for (i=1; i<9; i++)
 	{
@@ -58,11 +56,12 @@ while (1 == 1)
 		sprintf(convert, "%d", i);
 		strcat(dupu, convert);
 		int lineNb = 1;
+		char positioncurseur[255];
 
 		FILE *  date;
 		date = popen(dupu, "r");
 		while (fgets(tabdate, sizeof tabdate,date)) //fgets lit tout ce qui ce trouve dans date (le flux) et le place dans tabdate (le 1er). le sizeof tabdate sert a dire le maximum de caractére lu (ici la taille d'un tableau de 256)
-                	sertarien = sertarien;
+                	continue;
 
 
 		int qq = strlen(tabdate);
@@ -82,27 +81,20 @@ while (1 == 1)
 		{
 			if(lineNb > 2)
 			{
-				printHorizontal(line, W_CONSOLE);
+				printHorizontal(line, w_console);
 				printf("\n");
 			}
 			lineNb++;
-
 		}
-
+	w_console = w_console + 15;
 	} //for
-
-
-	char car = '.';
-	int nb = 0;
+	printf("Cet écran sera actualisé dans quelques secondes ");
+	fflush(stdout);
 	for (chrono = 10; chrono !=0; chrono--)
 	{
-		char phrase[255] = {'\0'};
-		char test[255];
-		strcat(phrase, "cela va s'actualiser dans 10 secondes");
-		printf("%s ", phrase);
-		affichernbrcar(nb, car);
-		printf("\r");
 		sleep(1);
+		printf(".");
+		fflush(stdout);
 	}
 
 	fclose(dessin);
@@ -111,8 +103,7 @@ while (1 == 1)
 }//while
 }//main
 
-FILE* chargeImg(FILE *descriptor, char *path, char *mode)
-{
+FILE* chargeImg(FILE *descriptor, char *path, char *mode) {
 	int pid;
 
 	pid = fork();
@@ -135,8 +126,7 @@ FILE* chargeImg(FILE *descriptor, char *path, char *mode)
 	return descriptor;
 }
 
-void sizeImg(char *l, int *h, int *w)
-{
+void sizeImg(char *l, int *h, int *w) {
 	char *tmp;
 
 	tmp = strtok(l, " ");
@@ -148,8 +138,7 @@ void sizeImg(char *l, int *h, int *w)
 	*h = atoi(tmp);
 }
 
-void printHorizontal(char *l, size_t wei)
-{
+void printHorizontal(char *l, size_t wei) {
 	size_t lenght = strlen(l);
 
 	if(wei >= lenght)
@@ -177,8 +166,7 @@ void printHorizontal(char *l, size_t wei)
 	}
 }
 
-void center(int hMax, int *h) 
-{
+void center(int hMax, int *h) {
 	if(hMax >= *h)
 	{
 		int n = (hMax - *h)/2;
@@ -193,12 +181,4 @@ void center(int hMax, int *h)
 		printf("Image trop grande");
 	}
 
-}
-
-
-void affichernbrcar(int repet, char caractere)
-{
-int i;
-for(i=0; i<repet; i++)
-	printf("%c", caractere);
 }
